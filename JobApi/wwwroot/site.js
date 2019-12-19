77d524c749853fffe1ba3e6799f5c29638593dfc
lindkvist71 <71spettet@gmail.com>
2019-12-19 11:19:56 +0100

    Justering av text i popupruta

--- JobApi/wwwroot/site.js
+++ JobApi/wwwroot/site.js
@@ -1,11 +1,9 @@
 ﻿const uri = "api/Job";
 let jobs = null;
-
 //hämtar information via interna api om antal objekt som finns med i databasen
 function getCount(data) {
     const el = $("#counter");
     let name = "lediga tjänster";
-
     //kontrollerar antalet annonser och skriver ut rätt ord / även om det inte finns några annonser
     if (data) {
         if (data <= 1) {
@@ -16,10 +14,8 @@ function getCount(data) {
         el.text("Inga " + name);
     }
 }
-
 //När sidan är färdigladdad så kör den funktionen getData()
 $(document).ready(function () {
-
 
     $(".searchBtn").on("click", function () {
         getData();
@@ -35,7 +31,6 @@ $(document).ready(function () {
         closeView();
     });
 });
-
 //hämtar information via interna apiet och skriver ut det i en lista//
 function getData() {
     $.ajax({
@@ -48,13 +43,10 @@ function getData() {
             $(tBody).empty();
 
             getCount(data.length);
-
             //loopar igenom varje object
             $.each(data, function (key, item) {
                 const tr = $("<tr></tr>")
-
                     //lägga till dom hämtade fält från classen JobItem i c#
-                    //.append($("<td></td>").text(item.id))
                     .append($("<td></td>").text(item.title))
                     .append($("<td></td>").text(item.dateEnd))
                     .append($("<td></td>").text(item.place))
@@ -64,24 +56,18 @@ function getData() {
                     viewItem(item.id);
                 });
             });
-
             jobs = data;
         }
     });
-
 }
-
 //visar ytterligare information om annonsen
 function viewItem(id) {
     $.each(jobs, function (key, item) {
         if (item.id === id) {
-            //$("#view-id").text(id);
             $("#view-title").text(item.title);
             $("#view-place").text(item.place);
             $("#view-region").text(item.region);
-
             $("#view-text").text(item.text);
-
             $("#view-dateEnd").text(item.dateEnd);
         }
     });
@@ -89,7 +75,6 @@ function viewItem(id) {
     $("#overlay").css({ display: "block" });
     $("#view").css({ display: "block" });
     $("#view").css({ display: "block" });
-
     $("body").css({ overflow: "hidden" });
 }
 
